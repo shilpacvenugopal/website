63adf741c66a6a5cd745b466c8ebb428c04d1917
shilpacvenugopal <shilpacvenugopal1997@gmail.com>
2025-11-07 19:27:42 +0530

    change on setting ie db

--- BYN/settings.py
+++ BYN/settings.py
@@ -107,12 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Internationalization
